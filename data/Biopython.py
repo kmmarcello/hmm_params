5f1ad33bd5803954f188f1bfec3c8caad7afa8ab
kmmarcello <kmmarcello@ucdavis.edu>
2022-06-17 09:41:26 -0700

    edits

--- data/Biopython.py
+++ data/Biopython.py
@@ -1,9 +1,14 @@
-from Bio.Seq import Seq
+"""from Bio.Seq import Seq
 dna = Seq("ATGCATGCGCTATCGCATGCGTCATG")
 print(dna)
+"""
+
 
 from Bio import SeqIO
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
-for sequence in SeqIO.parse("desa-trimmed-no-microcollius.fa", "fasta"):
+
+for sequence in SeqIO.parse("desA-concat-filamentous.fa", "fasta"):
     print(sequence.id)
-    #print(sequence.count_amino_acids()['A']) 
+    xSeq = ProteinAnalysis(sequence.seq)
+    print(xSeq.count_amino_acids()['A']) 
+    
